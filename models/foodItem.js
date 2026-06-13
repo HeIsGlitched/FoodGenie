7f4c377e7daa3ece6d38bd8e0144d3bd57922034
Divyanshu <divyanshu.4987@gmail.com>
2026-06-13 11:35:01 +0530

    Extend restaurant API with menu and food items

---
+++ models/foodItem.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose")
+
+const foodSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: [true,"Please enter food item name"],
+        trim : true,
+        maxLength:[100,"FoodItem name cannot be more than 100 chars"]
+    },
+    price:{
+        type: Number,
+        required:[true,"Please enter price"],
+        maxLength:[5,"Item price cannot be more than 5 char"],
+        default:0.0
+    },
+    description:{
+        type: String,
+        required:[true,"Please enter description"]
+    },
+    ratings:{
+        type: Number,
+        default: 0
+    },
+    images:{
+        public_id:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+    },
+    menu:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Menu"
+    },
+    stock:{
+        type:Number,
+        required:[true,"Please enter quantity of items"],
+        maxLength:[5,"FoodItems stock cannot be more than 5 char"],
+        default:0
+    },
+    restaurant:{
+        type:mongoose.Schema.Types.ObjectId;
+        ref:"Restaurant"
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        defualt:Date.now()
+    }
+})
+
+module.exports = mongoose.model("FoodItem", foodSchema)
